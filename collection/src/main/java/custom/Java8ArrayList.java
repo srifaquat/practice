0d7d9aa84217411b96e49057fb2dd683a2fe6746
srifaquat <srifaquat@gmail.com>
2021-11-15 17:58:03 +0530

    added arraylist java 8 program

--- collection/src/main/java/custom/Java8ArrayList.java
+++ collection/src/main/java/custom/Java8ArrayList.java
@@ -4,11 +4,21 @@ import model.Employee;
 import utils.EmployeeDataGenerator;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Java8ArrayList {
 
     public static void main(String[] args) {
         List<Employee> list = EmployeeDataGenerator.generateData(10);
-        
+        //Grouping employees by departmentId
+        Map<Integer,List<Employee>> departmentEmployees = list.stream().collect(Collectors.groupingBy(Employee::getDepartmentId, Collectors.toList()));
+
+        //Count employees by deparmentId
+        Map<Integer,Long> departmentEmployeesCount = list.stream().collect(Collectors.groupingBy(Employee::getDepartmentId, Collectors.counting()));
+
+        System.out.println(departmentEmployees);
+
     }
 }
