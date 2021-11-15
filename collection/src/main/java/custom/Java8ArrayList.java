7edd508f0252792497c167766f6e8b2a8190d9c5
srifaquat <srifaquat@gmail.com>
2021-11-15 18:01:32 +0530

    filtering by department

--- collection/src/main/java/custom/Java8ArrayList.java
+++ collection/src/main/java/custom/Java8ArrayList.java
@@ -12,11 +12,22 @@ public class Java8ArrayList {
 
     public static void main(String[] args) {
         List<Employee> list = EmployeeDataGenerator.generateData(10);
+
+        final Integer departmentId = list.get(3).getDepartmentId();
+
+
         //Grouping employees by departmentId
         Map<Integer,List<Employee>> departmentEmployees = list.stream().collect(Collectors.groupingBy(Employee::getDepartmentId, Collectors.toList()));
 
+
         //Count employees by deparmentId
         Map<Integer,Long> departmentEmployeesCount = list.stream().collect(Collectors.groupingBy(Employee::getDepartmentId, Collectors.counting()));
+
+
+        //Filter to fetch list of employee for only department Id {departmentId variable}
+        Map<Integer,List<Employee>> departmentEmployeesFilter = list.stream()
+                .filter(employee -> employee.getDepartmentId() == departmentId)
+                .collect(Collectors.groupingBy(Employee::getDepartmentId, Collectors.toList()));
 
         System.out.println(departmentEmployees);
 
